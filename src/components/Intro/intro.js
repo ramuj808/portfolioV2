df56da95bd237dac6be8add54bbc95c33f9d62d4
Jumar Pantoca <jpantoca@gmail.com>
2024-01-24 10:05:19 -1000

    Added an introduction component

---
+++ src/components/Intro/intro.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./intro.css";
+
+const Intro = () => {
+  return (
+    <section id="intro">
+      <div className="introContent"></div>
+      <img src="" alt="" />
+    </section>
+  );
+};
+
+export default Intro;
