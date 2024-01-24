672a167858898ee3364ec96ed205cd235e6c47ff
Jumar Pantoca <jpantoca@gmail.com>
2024-01-24 11:58:32 -1000

    Updated intro section with profile image and style

--- src/components/Intro/intro.js
+++ src/components/Intro/intro.js
@@ -1,11 +1,29 @@
 import React from "react";
+import { Link } from "react-scroll";
 import "./intro.css";
+import ResumeBtn from "../../assets/icons8-resume-48.png";
+import profile from "../../assets/IMG_6521.jpg";
 
 const Intro = () => {
   return (
     <section id="intro">
-      <div className="introContent"></div>
-      <img src="" alt="" />
+      <div className="introContent">
+        <span className="hello">Hi,</span>
+        <span className="introMessage">
+          My name is <span className="introName">Jumar</span> <br />
+        </span>
+        <p className="introPara">
+          I am a motivated and passionate web designer, expanding my learning to
+          creating things on the web.
+        </p>
+        <Link>
+          <button className="btn">
+            <img src={ResumeBtn} alt="Resume" />
+            Resume
+          </button>
+        </Link>
+      </div>
+      <img src={profile} alt="profile" className="bg-profile" />
     </section>
   );
 };
