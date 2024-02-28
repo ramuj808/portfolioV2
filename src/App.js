16520c254766c997ea6c32b606ed855a5754e73b
Jumar Pantoca <jpantoca@gmail.com>
2024-02-28 09:33:57 -1000

    Fixed navbar import

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import Intro from "./components/Intro/intro";
-import NavBar from "./components/NavBar/NavBar.js";
+import NavBar from "./components/NavBar/NavBar";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
-
       <Intro />
     </div>
   );
